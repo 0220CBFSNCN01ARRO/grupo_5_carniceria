95c1889d28c6a1178f7b40e73657ca0d43a0e7d5
Pedro Angel Yapur <Pedro@MBP-Peter.fibertel.com.ar>
2020-05-27 12:32:48 -0300

    modif

--- MeatMe/controllers/productController.js
+++ MeatMe/controllers/productController.js
@@ -10,41 +10,51 @@ let pollo = productosData.filter(producto => producto.category == "pollo");
 
 
 module.exports = {
-    show: (req,res, next) => {
-        res.render("products", { productosData })
-    },
-    byCategory: (req, res) => {
-        let categoria = req.params.category;
-        let productos = null;
+  show: (req, res, next) => {
+    res.render("products", { productosData })
+  },
+  byCategory: (req, res) => {
+    let categoria = req.params.category;
+    let productos = null;
 
-        if( categoria == "vacuno"){
-          productos = vacuno;
-          console.log(productos)
-        }
-        if( categoria == "cerdo"){
-        productos = cerdo;
-        }
-        if( categoria == "pollo"){
-        productos = pollo;
-        }
-        if (productos){
-        res.render("productByCategory", { productos, categoria })
-        } else {
-          let error =  {
-            message: 'Producto no encontrado',
-            error: {
-                status: 404,
-                stack: ''
-            }
-          }
-        }
-    },
-    detail: (req, res) => {
-        let  id = req.params.id;
-        let producto = productosData.find( prod => prod.id == id);
-        res.render("productDetail", { producto })
-    },
-    cart: (req, res) => {
-        res.render("productCart")
+    if (categoria == "vacuno") {
+      productos = vacuno;
+      console.log(productos)
     }
+    if (categoria == "cerdo") {
+      productos = cerdo;
+    }
+    if (categoria == "pollo") {
+      productos = pollo;
+    }
+    if (productos) {
+      res.render("productByCategory", { productos, categoria })
+    } else {
+      let error = {
+        message: 'Producto no encontrado',
+        error: {
+          status: 404,
+          stack: ''
+        }
+      }
+    }
+  },
+  detail: (req, res) => {
+    let id = req.params.id;
+    let producto = productosData.find(prod => prod.id == id);
+    res.render("productDetail", { producto })
+  },
+  cart: (req, res) => {
+    res.render("productCart")
+  },
+
+  // Create - Form to create
+  create: (req, res) => {
+    res.render('productAdd');
+  },
+
+
+
 }
+
+
