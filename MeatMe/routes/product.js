d9ecdbbb7029a513d12c710b2b779e7d198374df
Pedro Angel Yapur <Pedro@MBP-Peter.fibertel.com.ar>
2020-05-27 11:41:19 -0300

    modifique routes

--- MeatMe/routes/product.js
+++ MeatMe/routes/product.js
@@ -8,6 +8,16 @@ router.get("/cart", controllers.cart);
 router.get("/:category", controllers.byCategory);
 router.get("/:category/detail/:id", controllers.detail);
 
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/create/', controllers.create); /* GET - Form to create */
+router.post('/create/', controllers.store); /* POST - Store in DB */
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id', controllers.edit); /* GET - Form to create */
+router.put('/edit/:id', controllers.update); /* PUT - Update in DB */
+
+/*** DELETE ONE PRODUCT***/ 
+router.delete('/delete/:id', controllers.destroy); /* DELETE - Delete from DB */
 
 
 
