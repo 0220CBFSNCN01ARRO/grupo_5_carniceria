849db862387df492e97cd12fc8db11d45e6426ae
Pedro Angel Yapur <Pedro@MacBook-Pro-Peter.local>
2020-06-10 11:05:48 -0300

    modifique users

--- MeatMe/routes/users.js
+++ MeatMe/routes/users.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var controllers = require("../controllers/usersController");
+const multer = require('multer');
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+          cb(null, path.join(__dirname,'../../public/images/avatars'))
+    },
+    filename: (req, file, cb) => {
+          cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+  var upload = multer({ storage: storage });
+
 
 /* GET users listing. */
 router.get('/register', controllers.register);
