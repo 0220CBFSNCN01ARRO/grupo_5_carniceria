eb814eb3a8f9671cb08202e17f0b666e9eec84e5
Pedro Angel Yapur <Pedro@MacBook-Pro-Peter.local>
2020-06-10 11:01:26 -0300

    require multer,path, bcrypt

--- MeatMe/routes/index.js
+++ MeatMe/routes/index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var controllers = require("../controllers/indexController");
+const multer = require('multer');
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+          cb(null, path.join(__dirname,'../../public/images/avatars'))
+    },
+    filename: (req, file, cb) => {
+          cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  })
+  var upload = multer({ storage: storage });
+
 
 /* GET home page. */
 router.get('/', controllers.index);
+
 
 module.exports = router;
