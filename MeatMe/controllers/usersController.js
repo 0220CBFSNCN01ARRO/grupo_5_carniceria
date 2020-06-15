e3a346228724b8e3e380b05164c9c1dcb86d2012
Victor <victor_alagna@hotmail.com>
2020-06-15 15:28:35 -0300

    agrego el POST de la registracion y el multer al formulario

--- MeatMe/controllers/usersController.js
+++ MeatMe/controllers/usersController.js
@@ -1,11 +1,58 @@
+const fs = require ('fs');
+const path = require ('path');
+const bcrypt = require ('bcrypt');
+let usersPath = path.join(__dirname, '../data/usuarios.json');
+
+function getUsers (){
+    let userContent= fs.readFileSync (usersPath, 'utf-8')
+    return userContent != '' ? JSON.parse (userContent) : []
+}
+function generarID () {
+    let usuarios= getUsers();
+    if (usuarios.length){
+        let ids = usuarios.map((user) => user.id);
+        return Math.max (...ids) + 1;
+    } else{
+        return 1
+    };
+};
+function guardarUsuario (usuario) {
+    let usuarios = getUsers();
+    usuarios.push (usuario);
+    fs.writeFileSync(usersPath, JSON.stringify(usuarios, null, " "));
+}
+
+
+
+
 module.exports = {
     register: (req, res) => {
         res.render("register");
+    },
+
+    store: (req, res, next) => {
+        req.body.password = bcrypt.hashSync(req.body.password,10);
+        let usuarioNuevo= {
+            id: generarID(),
+            ...req.body,
+            avatar: req.files[0].filename
+        }
+        guardarUsuario (usuarioNuevo);
+        res.redirect ('/')
+
+        //res.send('guardar')
+
+
+
     },
     admin: (req, res) => {
         res.render("productAdd");
     },
     login: (req, res) => {
         res.render("login");
+    },
+    processLogin: (req, res, next) => {
+        res.send('verificado')
+
     }
 }
--- MeatMe/routes/users.js
+++ MeatMe/routes/users.js
@@ -6,19 +6,22 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 
 var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-          cb(null, path.join(__dirname,'../../public/images/avatars'))
-    },
-    filename: (req, file, cb) => {
-          cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-    }
-  })
-  var upload = multer({ storage: storage });
+      destination: (req, file, cb) => {
+            cb(null, path.join(__dirname,'../public/img/avatars'))
+      },
+      filename: (req, file, cb) => {
+            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+      }
+    })
+    var upload = multer({ storage: storage })
 
 
 /* GET users listing. */
 router.get('/register', controllers.register);
+router.post('/register',upload.any(), controllers.store);
 router.get("/admin", controllers.admin);
 router.get("/login", controllers.login);
+router.post("/login", controllers.processLogin)
+
 
 module.exports = router;
