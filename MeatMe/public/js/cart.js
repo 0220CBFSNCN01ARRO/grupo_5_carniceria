f5688f03722305ff04a95e6239645f577a9e6a37
Ramiro <ramazzucco@hotmail.com>
2020-08-06 19:24:25 -0300

    funcionalidad de carrito

--- MeatMe/public/js/cart.js
+++ MeatMe/public/js/cart.js
@@ -1,10 +1,47 @@
-const total = document.querySelectorAll("td .precio")
-const subTotal = total.forEach(precio => {
-console.log(precio.innerText)
-    return Number(precio.innerText)+Number(precio.innerText);
-});
-console.log(subTotal)
+window.onload = () => {
 
-// const precioTotal = subTotal.forEach(precio => {
-//     return precio+precio
-// })
+    function obtenerTotal() {
+        let precioTotal = 0;
+        const subTotales = document.querySelectorAll(".precio")
+        subTotales.forEach(subTotal => {
+            precioTotal += Number(subTotal.innerHTML)
+        })
+
+        return precioTotal;
+
+    }
+
+    const tr = document.querySelectorAll(".item")
+    console.log(tr)
+
+    if (tr != null) {
+
+        const total = document.querySelector(".precio-total")
+        const subTotales = document.querySelectorAll(".precio")
+
+
+        subTotales.forEach( precio => {
+        console.log(precio.innerHTML)
+
+            total.innerHTML = Number(total.innerHTML) + Number(precio.innerHTML)
+
+            tr.forEach( cant => {
+
+                const cantidad = cant.querySelector(".cart-type")
+                const subTotal = cant.querySelector(".precio")
+
+                cantidad.onchange = () => {
+
+                    const precio = cant.querySelector(".title p")
+                    subTotal.innerHTML = Number(precio.innerHTML) * Number(cantidad.value)
+                    document.querySelector(".precio-total").innerHTML = obtenerTotal()
+
+                }
+
+            })
+
+
+        })
+
+    }
+}
