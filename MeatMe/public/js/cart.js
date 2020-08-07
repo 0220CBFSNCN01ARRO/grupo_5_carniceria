de95f7e834996e54486ea3adeb11d61e68427457
Ramiro <ramazzucco@hotmail.com>
2020-08-07 20:25:46 -0300

    agregue la eliminacion de los items en el carrito

--- MeatMe/public/js/cart.js
+++ MeatMe/public/js/cart.js
@@ -1,47 +1,54 @@
 window.onload = () => {
+  function obtenerTotal() {
+    let precioTotal = 0;
+    const subTotales = document.querySelectorAll(".precio");
+    subTotales.forEach((subTotal) => {
+      precioTotal += Number(subTotal.innerHTML);
+    });
 
-    function obtenerTotal() {
-        let precioTotal = 0;
-        const subTotales = document.querySelectorAll(".precio")
-        subTotales.forEach(subTotal => {
-            precioTotal += Number(subTotal.innerHTML)
-        })
+    return precioTotal;
+  }
 
-        return precioTotal;
+  const tr = document.querySelectorAll(".item");
+  console.log(tr);
 
-    }
+  if (tr != null) {
+    const total = document.querySelector(".precio-total");
+    const subTotales = document.querySelectorAll(".precio");
 
-    const tr = document.querySelectorAll(".item")
-    console.log(tr)
+    subTotales.forEach((precio) => {
+      console.log(precio.innerHTML);
 
-    if (tr != null) {
+      total.innerHTML = Number(total.innerHTML) + Number(precio.innerHTML);
 
-        const total = document.querySelector(".precio-total")
-        const subTotales = document.querySelectorAll(".precio")
+      tr.forEach((cant) => {
+        const cantidad = cant.querySelector(".cart-type");
+        const subTotal = cant.querySelector(".precio");
 
+        cantidad.onchange = () => {
+          const precio = cant.querySelector(".title p");
+          subTotal.innerHTML =
+            Number(precio.innerHTML) * Number(cantidad.value);
+          document.querySelector(".precio-total").innerHTML = obtenerTotal();
+        };
+      });
+    });
+  }
 
-        subTotales.forEach( precio => {
-        console.log(precio.innerHTML)
+  const botonEliminar = document.querySelectorAll(".eliminar");
 
-            total.innerHTML = Number(total.innerHTML) + Number(precio.innerHTML)
+  botonEliminar.forEach((boton) => {
+    boton.onclick = () => {
+      let id = boton.getAttribute("data-id");
+      document.getElementById(`${id}`).innerHTML = "";
+      const subTotales = document.querySelectorAll(".precio");
+      const total = document.querySelector(".precio-total");
 
-            tr.forEach( cant => {
-
-                const cantidad = cant.querySelector(".cart-type")
-                const subTotal = cant.querySelector(".precio")
-
-                cantidad.onchange = () => {
-
-                    const precio = cant.querySelector(".title p")
-                    subTotal.innerHTML = Number(precio.innerHTML) * Number(cantidad.value)
-                    document.querySelector(".precio-total").innerHTML = obtenerTotal()
-
-                }
-
-            })
-
-
-        })
-
-    }
-}
+      subTotales.forEach(precio => {
+          console.log(precio.innerHTML)
+        total.innerHTML = 0
+        total.innerHTML = Number(total.innerHTML) + Number(precio.innerHTML);
+      });
+    };
+  });
+};
