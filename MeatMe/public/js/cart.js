2f8635dea31147f5c8355ef3c127d6bf6422def6
Pedro Angel Yapur <Pedro@MacBook-Pro-Peter.local>
2020-08-10 18:22:21 -0300

    subo modif vista index

--- MeatMe/public/js/cart.js
+++ MeatMe/public/js/cart.js
@@ -51,4 +51,40 @@ window.onload = () => {
       });
     };
   });
+  let url = location.href.slice(21);
+
+    switch (url) {
+        case "/":
+            document.querySelector(`.nav-item.inicio`).classList.add("active");
+            break;
+        case "/product":
+            document
+                .querySelector(`.nav-item.productos`)
+                .classList.add("active");
+            break;
+        case "/empresa":
+            document.querySelector(`.nav-item.empresa`).classList.add("active");
+            break;
+        case "/recetas":
+            document.querySelector(`.nav-item.recetas`).classList.add("active");
+            break;
+        case "/product/cart":
+            document.querySelector(`.nav-item.carrito`).classList.add("active");
+            break;
+        case "/users/register":
+            document
+                .querySelector(`.nav-item.registrarse`)
+                .classList.add("active");
+            break;
+        case "/users/login":
+            document
+                .querySelector(`.nav-item.loguearse`)
+                .classList.add("active");
+            break;
+        default:
+            document
+                .querySelector(".nav-item.active")
+                .classList.remove("active");
+            break;
+    }
 };
